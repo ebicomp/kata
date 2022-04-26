c3aff9fbaf75548dc993a151add0b9bdb73098c0
Abolfazl Karimi <ab.karimi@cbi.ir>
2022-04-26 13:48:17 +0430

    Create ConjuredItem class for the new requirement

---
+++ GildedRose/ConjuredItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseKata
+{
+    public class ConjuredItem : BaseItem
+    {
+        public ConjuredItem(Item item) : base(item) 
+        {
+        }
+        public override void Update()
+        {
+            DecreaseQuality();
+            DecreaseQuality();
+            DecreaseSellIn();
+            if (Item.SellIn < 0)
+            {
+                DecreaseQuality();
+            }
+        }
+    }
+}

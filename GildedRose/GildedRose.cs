c766cce6af28e4a52973157664e2f2b5d958ef45
Abolfazl Karimi <ab.karimi@cbi.ir>
2022-04-26 10:26:24 +0430

    Extract ItemCalculator class from GildedRose
    Based on primitive obsession/extract class philosopyh: when many functions depend on 1 argument, it indicates that a class could be extracted to reduce messages between functions and to take advantage of benefits of oop

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -4,83 +4,34 @@ namespace GildedRoseKata
 {
     public class GildedRose
     {
+        private const string Brie = "Aged Brie";
+        private const string Backstage = "Backstage passes to a TAFKAL80ETC concert";
+        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+
         IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
         }
-        private const string Brie = "Aged Brie";
-        private const string Backstage = "Backstage passes to a TAFKAL80ETC concert";
-        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
-        private const int MinQuality = 0;
-        private const int MaxQuality = 50;
-
-        private void DecreaseQuality(Item item)
-        {
-            if(item.Quality > MinQuality)
-                item.Quality -= 1;
-        }
-        private void IncreaseQuality(Item item)
-        {
-            if (item.Quality < MaxQuality)
-                item.Quality += 1;
-        }
-        private void DecreaseSellIn(Item item)
-        {
-            item.SellIn = item.SellIn - 1;
-        }
-        private void UpdateBrie(Item item)
-        {
-            IncreaseQuality(item);
-            DecreaseSellIn(item);
-            if (item.SellIn < 0)
-            {
-                IncreaseQuality(item);
-            }
-        }
-        private void UpdateBackStage(Item item)
-        {
-            IncreaseQuality(item);
-            if (item.SellIn < 11)
-            {
-                IncreaseQuality(item);
-            }
-            if (item.SellIn < 6)
-            {
-                IncreaseQuality(item);
-            }
-            DecreaseSellIn(item);
-            if (item.SellIn < 0)
-            {
-                item.Quality = 0;
-            }
-        }
-        private void UpdateNormalItem(Item item)
-        {
-            DecreaseQuality(item);
-            DecreaseSellIn(item);
-            if (item.SellIn < 0)
-            {
-                DecreaseQuality(item);
-            }
-        }
+       
 
         public void UpdateQuality()
         {
             foreach (var item in Items)
             {
+                var itemCalculator = new ItemCalculator(item);
                 switch (item.Name)
                 {
                     case Brie:
-                        UpdateBrie(item);
+                        itemCalculator.UpdateBrie();
                         break;
                     case Backstage:
-                        UpdateBackStage(item);
+                        itemCalculator.UpdateBackStage();
                         break;
                     case Sulfuras:
                         break;
                     default:
-                        UpdateNormalItem(item);
+                        itemCalculator.UpdateNormalItem();
                         break;
                 }
             }
---
+++ GildedRose/ItemCalculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseKata
+{
+    class ItemCalculator
+    {
+        private Item Item;
+        public ItemCalculator(Item item)
+        {
+            this.Item = item;
+        }
+
+        private const int MinQuality = 0;
+        private const int MaxQuality = 50;
+
+        private void DecreaseQuality()
+        {
+            if (Item.Quality > MinQuality)
+                Item.Quality -= 1;
+        }
+        private void IncreaseQuality()
+        {
+            if (Item.Quality < MaxQuality)
+                Item.Quality += 1;
+        }
+        private void DecreaseSellIn()
+        {
+            Item.SellIn = Item.SellIn - 1;
+        }
+        public void UpdateBrie()
+        {
+            IncreaseQuality();
+            DecreaseSellIn();
+            if (Item.SellIn < 0)
+            {
+                IncreaseQuality();
+            }
+        }
+        public void UpdateBackStage()
+        {
+            IncreaseQuality();
+            if (Item.SellIn < 11)
+            {
+                IncreaseQuality();
+            }
+            if (Item.SellIn < 6)
+            {
+                IncreaseQuality();
+            }
+            DecreaseSellIn();
+            if (Item.SellIn < 0)
+            {
+                Item.Quality = 0;
+            }
+        }
+        public void UpdateNormalItem()
+        {
+            DecreaseQuality();
+            DecreaseSellIn();
+            if (Item.SellIn < 0)
+            {
+                DecreaseQuality();
+            }
+        }
+    }
+}

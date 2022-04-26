eb087daafd485455d89711b4b575bf96b62c02a3
Abolfazl Karimi <ab.karimi@cbi.ir>
2022-04-26 11:20:00 +0430

    Create ItemFactory Class
    Make Base Class open to extention - close to modification (Open/close) of Solid

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -4,36 +4,17 @@ namespace GildedRoseKata
 {
     public class GildedRose
     {
-        private const string Brie = "Aged Brie";
-        private const string Backstage = "Backstage passes to a TAFKAL80ETC concert";
-        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
-
         IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
         }
-       
-
         public void UpdateQuality()
         {
             foreach (var item in Items)
             {
-                var baseItem = new BaseItem(item);
-                switch (item.Name)
-                {
-                    case Brie:
-                        new BrieItem(item).Update();
-                        break;
-                    case Backstage:
-                        new BackStageItem(item).Update();
-                        break;
-                    case Sulfuras:
-                        break;
-                    default:
-                        new NormalItem(item).Update();
-                        break;
-                }
+                var ItemCalculator = new ItemFactory(item).GetItemClass();
+                ItemCalculator.Update();
             }
         }
     }
---
+++ GildedRose/ItemFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseKata
+{
+   
+    public class ItemFactory
+    {
+        private const string Brie = "Aged Brie";
+        private const string Backstage = "Backstage passes to a TAFKAL80ETC concert";
+        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+
+
+        private Item Item;
+        public ItemFactory(Item item)
+        {
+            this.Item = item;
+        }
+        public BaseItem GetItemClass()
+        {
+            switch (Item.Name)
+            {
+                case Brie:
+                    return new BrieItem(Item);
+                case Backstage:
+                    return new BackStageItem(Item);
+                case Sulfuras:
+                    return new BaseItem(Item);
+                default:
+                    return new NormalItem(Item);
+            }
+        }
+    }
+}
